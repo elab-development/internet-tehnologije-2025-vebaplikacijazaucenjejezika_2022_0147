a163535f502e64b2f461ead85d40957e88da3fc9
Boske123 <bozicstefan34@gmail.com>
2026-02-10 18:02:41 +0100

    App Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import CourseDetails from './pages/CourseDetails';
+import LessonDetails from './pages/LessonDetails';
+
 function App() {
-  return <>Duolingo Client</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/course/:courseId' element={<CourseDetails />} />
+        <Route path='/lesson/:lessonId' element={<LessonDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
